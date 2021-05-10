ca059a6fc927391b789e359d8ba48f113ce08017
taj-code <tajofisher4@gmail.com>
2021-05-09 17:14:23 -0700

    more styles and js scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
-  $("#").submit(function(event) {
+  $("form#add").submit(function(event) {
     event.preventDefault();
-    const name = $("input#name").val();
-    console.log(person1Input);
 
-    $("#text").show();
+    $("#userinfo").show();
   });
 });
